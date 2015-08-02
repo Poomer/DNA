4250a698e9c980060a84e845bede9d151d921ce8
Poom Poomer <poomering@gmail.com>
2015-08-02 13:45:01 +0700

    Basic sequence detector works properly

--- dna_sequence_detector.py
+++ dna_sequence_detector.py
@@ -11,7 +11,7 @@ seq_dict = {}
 
 #	def __init__(self):
 
-dna1 = dna.DNA(10)
+dna1 = dna.DNA(20)
 dna1.DnaGen()
 dna1.PrintDna()
 counter = 0
@@ -19,33 +19,45 @@ counter = 0
 
 #for acid in dna1.sequence:
 #print("ENTERING FOR LOOP ")
-#start_acid = dna1.sequence
-start_acid = 'AAAAAAAAACCAAAAAGGGGGGGGGGGAAAA'
+start_acid = dna1.sequence
+#start_acid = 'AAGCCCCCCGCACAATTTTTT'
 print("start_acid : " + start_acid)
 
 seq = ''
+first_pos = 0
+
 
 for pos in range(0,len(start_acid)-1):
 
-	#if(pos == len(start_acid)-1):					### Last acid of the sequence
-	#	break
-	#else:
-		if (start_acid[pos+1] == start_acid[pos]) and (len(seq)==0):    ### Plausible firstsequence detected
-			seq = start_acid[pos:pos+1]
-			print("first seq work")
+		if (start_acid[pos+1] == start_acid[pos]) and (len(seq)==0):    ### Plausible first sequence detected
+			seq = start_acid[pos]
+			print("at position %d : first seq work" % pos)
 			first_pos = pos
 			print(first_pos)
-			seq_l.append(pos)
-		elif(start_acid[pos+1] == start_acid[pos]):					### if continues, just add it
+			if(pos==len(start_acid)-2):                                 ### If it is the penultimate one, always add another acid.
+				seq +=seq
+			seq_dict[first_pos] = seq
+
+
+			## In case this is the last sequence
+
+		elif(start_acid[pos+1] == start_acid[pos]):						### if continues, just add it
 			seq += start_acid[pos+1]
-			print("continue seq")
+			print("at position %d : continue seq"% pos)
 			print(seq)
-		else:														### Reset seq and push it to seq_list
-			seq_l.append(seq)
-			print("seq end")
+			if(pos==len(start_acid)-2):                                 ### If it is the penultimate one, always add another acid.
+				seq +=start_acid[pos]
+			seq_dict[first_pos] = seq
+
+		
+			### Reset seq and push it to seq_list
+		else:														
+			if len(seq)!=0:
+				seq_dict[first_pos] = seq+seq[0]
+			print("at position %d : seq end"%pos)
 			seq = ''
 
-print(seq_l)
+print(seq_dict)
 
 
 
